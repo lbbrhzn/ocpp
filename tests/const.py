0a2f3830920d6215e287e321ce8b7bdfb5aff6f1
drc38 <derek_Caudwell@hotmail.com>
2021-07-08 22:26:58 +1200

    update mock_config with required params

--- tests/const.py
+++ tests/const.py
@@ -1,9 +1,19 @@
 """Constants for ocpp tests."""
-from custom_components.ocpp.const import CONF_HOST, CONF_NAME, CONF_PORT
+from custom_components.ocpp.const import (
+    CONF_CPID,
+    CONF_CSID,
+    CONF_HOST,
+    CONF_METER_INTERVAL,
+    CONF_NAME,
+    CONF_PORT,
+)
 
 MOCK_CONFIG = {
     CONF_NAME: "test_charger",
     CONF_HOST: "0.0.0.0",
     CONF_PORT: 9000,
+    CONF_CPID: "test_cpid",
+    CONF_CSID: "test_csid",
+    CONF_METER_INTERVAL: 60,
 }
 DEFAULT_NAME = "test"
