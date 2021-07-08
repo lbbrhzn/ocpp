323bc5ec50db1cf7be8d6afcabb9a4840192cb2a
drc38 <derek_Caudwell@hotmail.com>
2021-07-08 21:50:18 +1200

    match test_charge_point port

--- tests/const.py
+++ tests/const.py
@@ -4,6 +4,6 @@ from custom_components.ocpp.const import CONF_HOST, CONF_NAME, CONF_PORT
 MOCK_CONFIG = {
     CONF_NAME: "test_charger",
     CONF_HOST: "0.0.0.0",
-    CONF_PORT: 9123,
+    CONF_PORT: 9000,
 }
 DEFAULT_NAME = "test"
