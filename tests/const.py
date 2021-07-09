f6b2b4a8193781e99c0bd2ddd278df1b16990fc5
drc38 <derek_Caudwell@hotmail.com>
2021-07-09 12:21:51 +1200

    use vol.schema for mock_config_2 generation

--- tests/const.py
+++ tests/const.py
@@ -1,10 +1,13 @@
 """Constants for ocpp tests."""
+import voluptuous as vol
+
 from custom_components.ocpp.const import (
     CONF_CPID,
     CONF_CSID,
     CONF_HOST,
     CONF_METER_INTERVAL,
     CONF_PORT,
+    DEFAULT_MEASURAND,
     MEASURANDS,
 )
 
@@ -15,5 +18,10 @@ MOCK_CONFIG = {
     CONF_CSID: "test_csid",
     CONF_METER_INTERVAL: 60,
 }
-MOCK_CONFIG_2 = {MEASURANDS[1]: True}
+MOCK_CONFIG_2 = vol.Schema(
+    {
+        vol.Required(m, default=(True if m == DEFAULT_MEASURAND else False)): bool
+        for m in MEASURANDS
+    }
+)
 DEFAULT_NAME = "test"
