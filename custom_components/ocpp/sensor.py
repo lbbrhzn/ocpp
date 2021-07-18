1c65e14c973257de065bb805d29059c9e58e227f
drc38 <derek_Caudwell@hotmail.com>
2021-07-18 21:57:07 +1200

    ocpp: change to async update

--- custom_components/ocpp/sensor.py
+++ custom_components/ocpp/sensor.py
@@ -100,7 +100,6 @@ class ChargePointMetric(Entity):
             "integration": DOMAIN,
         }
 
-    async def update(self):
+    async def async_update(self):
         """Get the latest data and update the states."""
         self._state = self.central_system.get_metric(self.cp_id, self.metric)
-
