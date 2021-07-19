2f928f27399f1d896bb03606285bbfb7e3b94dd7
drc38 <derek_Caudwell@hotmail.com>
2021-07-20 00:14:05 +1200

    ocpp: modify process phases

--- custom_components/ocpp/api.py
+++ custom_components/ocpp/api.py
@@ -676,8 +676,9 @@ class ChargePoint(cp):
     def process_phases(self, data):
         """Process phase data from meter values payload."""
         extra_attr = {}
-        l1l2l3 = {}  # ordered Dict for each phase eg {0:{"unit":"V"},1:{"l1":"230"}...}
         for sv in data:
+            # ordered Dict for each phase eg {0:{"unit":"V"},1:{"L1":"230"}...}
+            l1l2l3 = {}
             if sv.get(om.phase.value) in [Phase.l1.value, Phase.l1_n.value]:
                 l1l2l3[1] = {sv.get(om.phase.value): float(sv[om.value.value])}
             if sv.get(om.phase.value) in [Phase.l2.value, Phase.l2_n.value]:
