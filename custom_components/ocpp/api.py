74838b60bf26382646c42ebb3694fb8a0556d111
lbbrhzn <wessel@lubberhuizen.net>
2021-07-23 17:14:52 +0200

    Update api.py
    Improve processing of voltage phases

--- custom_components/ocpp/api.py
+++ custom_components/ocpp/api.py
@@ -703,10 +703,7 @@ class ChargePoint(cp):
                     + value[Phase.l2_n.value]
                     + value[Phase.l3_n.value]
                 )
-                if sum > 0:
-                    self._metrics[metric] = round(sum / 3, 1)
-                else:
-                    self._metrics[metric] = round(sum, 1)
+                self._metrics[metric] = round(sum / 3, 1)
             if metric in [
                 Measurand.current_import.value,
                 Measurand.current_export.value,
