4b5d295a3ac93d029b5c29832a8da2a2135378a0
drc38 <20024196+drc38@users.noreply.github.com>
2021-11-09 14:31:54 +1300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,8 +29,8 @@ def skip_notifications_fixture():
 @pytest.fixture(name="bypass_get_data")
 def bypass_get_data_fixture():
     """Skip calls to get data from API."""
-    # with patch("custom_components.ocpp.ocppApiClient.async_get_data"):
-    yield
+    with patch("custom_components.ocpp.ocppApiClient.async_get_data"):
+        yield
 
 
 # In this fixture, we are forcing calls to async_get_data to raise an Exception. This is useful
