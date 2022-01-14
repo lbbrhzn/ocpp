93ac9dacb3cdfefa66fab24885ee24316dcec781
drc38 <20024196+drc38@users.noreply.github.com>
2022-01-15 06:00:41 +1300

    Add new config to tests

--- tests/const.py
+++ tests/const.py
@@ -3,6 +3,7 @@ from custom_components.ocpp.const import (
     CONF_CPID,
     CONF_CSID,
     CONF_HOST,
+    CONF_IDLE_INTERVAL,
     CONF_METER_INTERVAL,
     CONF_MONITORED_VARIABLES,
     CONF_PORT,
@@ -14,6 +15,7 @@ MOCK_CONFIG = {
     CONF_PORT: 9000,
     CONF_CPID: "test_cpid",
     CONF_CSID: "test_csid",
+    CONF_IDLE_INTERVAL: 900,
     CONF_METER_INTERVAL: 60,
 }
 MOCK_CONFIG_2 = {
@@ -45,6 +47,7 @@ MOCK_CONFIG_DATA = {
     CONF_PORT: 9000,
     CONF_CPID: "test_cpid",
     CONF_CSID: "test_csid",
+    CONF_IDLE_INTERVAL: 900,
     CONF_METER_INTERVAL: 60,
     CONF_MONITORED_VARIABLES: "Current.Export,Current.Import,Current.Offered,Energy.Active.Export.Register,Energy.Active.Import.Register,Energy.Reactive.Export.Register,Energy.Reactive.Import.Register,Energy.Active.Export.Interval,Energy.Active.Import.Interval,Energy.Reactive.Export.Interval,Energy.Reactive.Import.Interval,Frequency,Power.Active.Export,Power.Active.Import,Power.Factor,Power.Offered,Power.Reactive.Export,Power.Reactive.Import,RPM,SoC,Temperature,Voltage",
 }
@@ -54,6 +57,7 @@ MOCK_CONFIG_SWITCH = {
     CONF_PORT: 9001,
     CONF_CPID: "test_cpid_2",
     CONF_CSID: "test_csid_2",
+    CONF_IDLE_INTERVAL: 900,
     CONF_METER_INTERVAL: 60,
     CONF_MONITORED_VARIABLES: "Current.Export,Current.Import,Current.Offered,Energy.Active.Export.Register,Energy.Active.Import.Register,Energy.Reactive.Export.Register,Energy.Reactive.Import.Register,Energy.Active.Export.Interval,Energy.Active.Import.Interval,Energy.Reactive.Export.Interval,Energy.Reactive.Import.Interval,Frequency,Power.Active.Export,Power.Active.Import,Power.Factor,Power.Offered,Power.Reactive.Export,Power.Reactive.Import,RPM,SoC,Temperature,Voltage",
 }
