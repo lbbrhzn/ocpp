20e343ffae4bffd36dfd96a384a6530c58f7d8e0
drc38 <20024196+drc38@users.noreply.github.com>
2022-01-23 21:09:16 +1300

    Update test_charge_point.py

--- tests/test_charge_point.py
+++ tests/test_charge_point.py
@@ -199,8 +199,8 @@ async def test_cms_responses(hass, socket_enabled):
             await asyncio.wait_for(
                 asyncio.gather(
                     cp.start(),
-                    cs.trigger_boot_notification(),
-                    cs.trigger_status_notification(),
+                    cs.charge_points[cs.cpid].trigger_boot_notification(),
+                    cs.charge_points[cs.cpid].trigger_status_notification(),
                     test_switches(hass, socket_enabled),
                     test_services(hass, socket_enabled),
                     test_buttons(hass, socket_enabled),
