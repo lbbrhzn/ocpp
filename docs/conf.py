eb3eadef4aa8884c8ec8c675567bd951c8d359fa
lbbrhzn <@lbbrhzn>
2022-03-03 08:30:51 +0000

    prepare for markdown

--- docs/conf.py
+++ docs/conf.py
@@ -2,12 +2,7 @@
 
 # -- Project information
 
-project = "Lumache"
-copyright = "2021, Graziella"
-author = "Graziella"
-
-release = "0.1"
-version = "0.1.0"
+project = "homeassistant_ocpp"
 
 # -- General configuration
 
@@ -19,17 +14,16 @@ extensions = [
     "sphinx.ext.intersphinx",
     "myst_parser",
 ]
+source_suffix = [".rst", ".md"]
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
     "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
 intersphinx_disabled_domains = ["std"]
-
 templates_path = ["_templates"]
 
 # -- Options for HTML output
-
 html_theme = "sphinx_rtd_theme"
 
 # -- Options for EPUB output
