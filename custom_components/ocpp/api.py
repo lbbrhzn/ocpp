79326dcb9fe148219a974c714509a5dfe3fd2882
drc38 <20024196+drc38@users.noreply.github.com>
2022-04-02 17:22:42 +1300

    Update api.py

--- custom_components/ocpp/api.py
+++ custom_components/ocpp/api.py
@@ -479,7 +479,7 @@ class ChargePoint(cp):
         req = call.GetConfigurationPayload(key=[ckey.supported_feature_profiles.value])
         resp = await self.call(req)
         feature_list = (resp.configuration_key[0][om.value.value]).split(",")
-        if feature_list is None:
+        if feature_list[0] == "":
             _LOGGER.warning("No feature profiles detected, defaulting to Core")
             await self.notify_ha("No feature profiles detected, defaulting to Core")
             feature_list = [om.feature_profile_core.value]
