28f40a803599f6a5da0189ca250808fa06b07f63
drc38 <20024196+drc38@users.noreply.github.com>
2022-07-11 07:45:15 +1200

    change number to native (#558)
    * change number to native
    
    * update HA requirement
    
    * update python to 3.10
    
    * fix python env
    
    * Remove redundant code
    
    * Remove vol import

--- custom_components/ocpp/number.py
+++ custom_components/ocpp/number.py
@@ -8,12 +8,11 @@ from homeassistant.components.number import (
     DOMAIN as NUMBER_DOMAIN,
     NumberEntity,
     NumberEntityDescription,
+    RestoreNumber,
 )
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.helpers.restore_state import RestoreEntity
-import voluptuous as vol
 
 from .api import CentralSystem
 from .const import (
@@ -33,10 +32,6 @@ class OcppNumberDescription(NumberEntityDescription):
     """Class to describe a Number entity."""
 
     initial_value: float | None = None
-    # can be removed when dev branch released
-    max_value: float | None = None
-    min_value: float | None = None
-    step: float | None = None
 
 
 NUMBERS: Final = [
@@ -45,9 +40,9 @@ NUMBERS: Final = [
         name="Maximum_Current",
         icon=ICON,
         initial_value=DEFAULT_MAX_CURRENT,
-        min_value=0,
-        max_value=DEFAULT_MAX_CURRENT,
-        step=1,
+        native_min_value=0,
+        native_max_value=DEFAULT_MAX_CURRENT,
+        native_step=1,
     ),
 ]
 
@@ -63,13 +58,13 @@ async def async_setup_entry(hass, entry, async_add_devices):
     for ent in NUMBERS:
         if ent.key == "maximum_current":
             ent.initial_value = entry.data.get(CONF_MAX_CURRENT, DEFAULT_MAX_CURRENT)
-            ent.max_value = entry.data.get(CONF_MAX_CURRENT, DEFAULT_MAX_CURRENT)
+            ent.native_max_value = entry.data.get(CONF_MAX_CURRENT, DEFAULT_MAX_CURRENT)
         entities.append(OcppNumber(hass, central_system, cp_id, ent))
 
     async_add_devices(entities, False)
 
 
-class OcppNumber(RestoreEntity, NumberEntity):
+class OcppNumber(RestoreNumber, NumberEntity):
     """Individual slider for setting charge rate."""
 
     entity_description: OcppNumberDescription
@@ -97,19 +92,15 @@ class OcppNumber(RestoreEntity, NumberEntity):
             identifiers={(DOMAIN, self.cp_id)},
             via_device=(DOMAIN, self.central_system.id),
         )
-        self._attr_value = self.entity_description.initial_value
-        # can be removed when dev branch released
-        self._attr_max_value = self.entity_description.max_value
-        self._attr_min_value = self.entity_description.min_value
-        self._attr_step = self.entity_description.step
+        self._attr_native_value = self.entity_description.initial_value
         self._attr_should_poll = False
         self._attr_available = True
 
     async def async_added_to_hass(self) -> None:
         """Handle entity which will be added."""
         await super().async_added_to_hass()
-        if state := await self.async_get_last_state():
-            self._attr_value = state.state
+        if restored := await self.async_get_last_number_data():
+            self._attr_native_value = restored.native_value
         async_dispatcher_connect(
             self._hass, DATA_UPDATED, self._schedule_immediate_update
         )
@@ -127,15 +118,9 @@ class OcppNumber(RestoreEntity, NumberEntity):
     #        return False
     #    return self.central_system.get_available(self.cp_id)  # type: ignore [no-any-return]
 
-    async def async_set_value(self, value):
+    async def async_set_native_value(self, value):
         """Set new value."""
         num_value = float(value)
-
-        if num_value < self._attr_min_value or num_value > self._attr_max_value:
-            raise vol.Invalid(
-                f"Invalid value for {self.entity_id}: {value} (range {self._attr_min_value} - {self._attr_max_value})"
-            )
-
         if self.central_system.get_available(
             self.cp_id
         ) and Profiles.SMART & self.central_system.get_supported_features(self.cp_id):
@@ -143,5 +128,5 @@ class OcppNumber(RestoreEntity, NumberEntity):
                 self.cp_id, num_value
             )
             if resp is True:
-                self._attr_value = num_value
+                self._attr_native_value = num_value
                 self.async_write_ha_state()
