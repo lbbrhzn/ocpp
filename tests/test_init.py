358ddf9da4f376e4e0b2496f983e83c792c5cfd6
drc38 <20024196+drc38@users.noreply.github.com>
2023-11-03 10:49:10 +1300

    revert setup change

--- tests/test_init.py
+++ tests/test_init.py
@@ -30,7 +30,7 @@ async def test_setup_unload_and_reload_entry(hass, bypass_get_data):
     # Set up the entry and assert that the values set during setup are where we expect
     # them to be. Because we have patched the ocppDataUpdateCoordinator.async_get_data
     # call, no code from custom_components/ocpp/api.py actually runs.
-    assert await hass.config_entries.async_setup(config_entry.entry_id)
+    assert await async_setup_entry(hass, config_entry)
     await hass.async_block_till_done()
 
     assert DOMAIN in hass.data and config_entry.entry_id in hass.data[DOMAIN]
