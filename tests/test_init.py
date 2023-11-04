380152b46f249518b10bc14c47b2ffa4df6d435c
lbbrhzn <8673442+lbbrhzn@users.noreply.github.com>
2023-11-04 14:23:46 +0000

    manually add mock entry to hass

--- tests/test_init.py
+++ tests/test_init.py
@@ -25,7 +25,8 @@ async def test_setup_unload_and_reload_entry(hass, bypass_get_data):
     config_entry = MockConfigEntry(
         domain=DOMAIN, data=MOCK_CONFIG_DATA_1, entry_id="test_cms1", title="test_cms1"
     )
-    config_entry.add_to_hass(hass)
+    #config_entry.add_to_hass(hass);
+    hass.config_entries._entries[config_entry.entry_id] = config_entry
 
     # Set up the entry and assert that the values set during setup are where we expect
     # them to be. Because we have patched the ocppDataUpdateCoordinator.async_get_data
