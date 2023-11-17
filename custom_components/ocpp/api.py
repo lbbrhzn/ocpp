beb052e94b70871edefb62ec98fda859960281bc
drc38 <20024196+drc38@users.noreply.github.com>
2023-11-17 17:12:31 +1300

    Assign mapped proxy

--- custom_components/ocpp/api.py
+++ custom_components/ocpp/api.py
@@ -447,7 +447,7 @@ class ChargePoint(cp):
                     accepted_measurands.append(measurand)
 
             accepted_measurands = ",".join(accepted_measurands)
-            updated_entry = self.entry.data
+            updated_entry = {**self.entry.data}
             updated_entry[CONF_MONITORED_VARIABLES] = accepted_measurands
             self.hass.config_entries.async_update_entry(self.entry, data=updated_entry)
 
