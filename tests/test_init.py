28e470fa723bcf66af5b171ac7df8a3cd7acb9ef
lbbrhzn <8673442+lbbrhzn@users.noreply.github.com>
2024-08-17 17:42:18 +0200

    Update test_init.py

--- tests/test_init.py
+++ tests/test_init.py
@@ -26,6 +26,7 @@ async def test_setup_unload_and_reload_entry(
         domain=DOMAIN, data=MOCK_CONFIG_DATA_1, entry_id="test_cms1", title="test_cms1"
     )
     config_entry.add_to_hass(hass)
+    await hass.async_block_till_done()
     
     # Set up the entry and assert that the values set during setup are where we expect
     # them to be. Because we have patched the ocppDataUpdateCoordinator.async_get_data
@@ -33,7 +34,8 @@ async def test_setup_unload_and_reload_entry(
     assert await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
 
-    assert DOMAIN in hass.data and config_entry.entry_id in hass.data[DOMAIN]
+    assert DOMAIN in hass.data 
+    assert config_entry.entry_id in hass.data[DOMAIN]
     assert type(hass.data[DOMAIN][config_entry.entry_id]) is CentralSystem
 
     # Reload the entry and assert that the data from above is still there
