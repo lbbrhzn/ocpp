351acea5859b56dbaee60e79a42eb491dea1e78b
lbbrhzn <8673442+lbbrhzn@users.noreply.github.com>
2024-08-17 17:44:01 +0200

    Update test_init.py

--- tests/test_init.py
+++ tests/test_init.py
@@ -33,8 +33,7 @@ async def test_setup_unload_and_reload_entry(
     # call, no code from custom_components/ocpp/api.py actually runs.
     assert await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
-
-    assert DOMAIN in hass.data 
+    assert DOMAIN in hass.data
     assert config_entry.entry_id in hass.data[DOMAIN]
     assert type(hass.data[DOMAIN][config_entry.entry_id]) is CentralSystem
 
