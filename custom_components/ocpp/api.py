65fcca14e2236c94bd4b8c35d5c71331f619c781
drc38 <20024196+drc38@users.noreply.github.com>
2024-09-26 14:11:41 +1200

    fix emty value

--- custom_components/ocpp/api.py
+++ custom_components/ocpp/api.py
@@ -1226,6 +1226,11 @@ class ChargePoint(cp):
                 phase = sampled_value.get(om.phase.value, None)
                 location = sampled_value.get(om.location.value, None)
                 context = sampled_value.get(om.context.value, None)
+                # where an empty string is supplied convert to 0
+                try:
+                    value = float(value)
+                except:
+                    value = 0
 
                 if len(sampled_value.keys()) == 1:  # Backwards compatibility
                     measurand = DEFAULT_MEASURAND
@@ -1240,7 +1245,7 @@ class ChargePoint(cp):
 
                 if phase is None:
                     if unit == DEFAULT_POWER_UNIT:
-                        self._metrics[measurand].value = float(value) / 1000
+                        self._metrics[measurand].value = value / 1000
                         self._metrics[measurand].unit = HA_POWER_UNIT
                     elif (
                         measurand == DEFAULT_MEASURAND
@@ -1248,27 +1253,25 @@ class ChargePoint(cp):
                     ):
                         if transaction_matches:
                             if unit == DEFAULT_ENERGY_UNIT:
-                                value = float(value) / 1000
+                                value = value / 1000
                                 unit = HA_ENERGY_UNIT
-                            self._metrics[csess.session_energy.value].value = float(
-                                value
-                            )
+                            self._metrics[csess.session_energy.value].value = value
                             self._metrics[csess.session_energy.value].unit = unit
                             self._metrics[csess.session_energy.value].extra_attr[
                                 cstat.id_tag.name
                             ] = self._metrics[cstat.id_tag.value].value
                         else:
                             if unit == DEFAULT_ENERGY_UNIT:
-                                value = float(value) / 1000
+                                value = value / 1000
                                 unit = HA_ENERGY_UNIT
-                            self._metrics[measurand].value = float(value)
+                            self._metrics[measurand].value = value
                             self._metrics[measurand].unit = unit
                     elif unit == DEFAULT_ENERGY_UNIT:
                         if transaction_matches:
-                            self._metrics[measurand].value = float(value) / 1000
+                            self._metrics[measurand].value = value / 1000
                             self._metrics[measurand].unit = HA_ENERGY_UNIT
                     else:
-                        self._metrics[measurand].value = float(value)
+                        self._metrics[measurand].value = value
                         self._metrics[measurand].unit = unit
                     if location is not None:
                         self._metrics[measurand].extra_attr[om.location.value] = (
