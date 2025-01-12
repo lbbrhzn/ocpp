95dc1e16d9e503fef14903446fa19ee3a516fa36
Serge <altserg@yahoo.com>
2025-01-12 15:56:31 +0100

    More elegant way to fix the issue. Now the Active.Import.Register sensor also gets updated

--- custom_components/ocpp/chargepoint.py
+++ custom_components/ocpp/chargepoint.py
@@ -772,24 +772,24 @@ class ChargePoint(cp):
                     # Charger reports Energy.Active.Import.Register directly as Session energy for transactions.
                     self._charger_reports_session_energy = True
 
-                if measurand == DEFAULT_MEASURAND and is_transaction and context == ReadingContext.transaction_begin.value:
-                    # Ignore energy report messages for Transaction.Begin context
-                    continue
-
                 if phase is None:
                     if (
                         measurand == DEFAULT_MEASURAND
                         and self._charger_reports_session_energy
                     ):
-                        if is_transaction:
-                            self._metrics[csess.session_energy.value].value = value
-                            self._metrics[csess.session_energy.value].unit = unit
-                            self._metrics[csess.session_energy.value].extra_attr[
-                                cstat.id_tag.name
-                            ] = self._metrics[cstat.id_tag.value].value
-                        else:
+                        if context != ReadingContext.transaction_begin.value:
+                        # Ignore messages with Transaction Begin context
+                            if is_transaction:
+                                self._metrics[csess.session_energy.value].value = value
+                                self._metrics[csess.session_energy.value].unit = unit
+                                self._metrics[csess.session_energy.value].extra_attr[
+                                    cstat.id_tag.name
+                                ] = self._metrics[cstat.id_tag.value].value
+                            # Always update sensor related to DEFAULT_MEASURAND
                             self._metrics[measurand].value = value
                             self._metrics[measurand].unit = unit
+                        else:
+                           continue
                     else:
                         self._metrics[measurand].value = value
                         self._metrics[measurand].unit = unit
