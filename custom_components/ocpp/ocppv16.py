c5b39320f9beb669c16fdbd0490066d67b973eed
paultheu <113731399+paultheu@users.noreply.github.com>
2026-09-16 10:53:53 +0200

    ocpp16: charger level id_tag sensor not updating on StartTransaction without Authorize
    When an OCPP 1.6 charger performs local authorization it skips the Authorize message and sends StartTransaction directly.
    The on_start_transaction handler now also sets charger-level (connector 0) id_tag and not only the connector-level id_tag

--- custom_components/ocpp/ocppv16.py
+++ custom_components/ocpp/ocppv16.py
@@ -1760,6 +1760,9 @@ class ChargePoint(cp):
             self.active_transaction_id = tx_id
             self._set_session_start(connector_id, time.time(), estimated=False)
             self._metrics[(connector_id, cstat.id_tag)].value = id_tag
+            # StartTransaction is not always preceded with an Authorize (local authorization by charger)
+            # Make sure charger-id_tag (connector 0) is also set with id_tag.
+            self._metrics[0][cstat.id_tag.value].value = id_tag
             self._metrics[(connector_id, cstat.stop_reason)].value = ""
             self._metrics[(connector_id, csess.transaction_id)].value = tx_id
             try:
